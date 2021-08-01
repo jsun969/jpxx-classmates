11de9c492bad437afe030621ca9e91c86a4774c6
jsun969 <i@jsun969.cn>
2021-08-01 21:49:11 +0800

    :sparkles: Add form basic operation logic

--- pages/add.tsx
+++ pages/add.tsx
@@ -20,8 +20,14 @@ import dayjs from 'dayjs';
 const years = Array.from({ length: dayjs().year() - 2020 + 1 }, (_, i: number) => 2020 + i).reverse();
 
 export default function Add() {
-  const [year, setYear] = useState<string>('');
+  const [name, setName] = useState<string>('');
   const [gender, setGender] = useState<string>('');
+  const [year, setYear] = useState<string>('');
+  const [_class, _setClass] = useState<number>();
+  const [school, setSchool] = useState<string>('');
+  const [qq, setQQ] = useState<string>('');
+  const [wechat, setWechat] = useState<string>('');
+  const [content, setContent] = useState<string>('');
 
   return (
     <>
@@ -30,7 +36,13 @@ export default function Add() {
       </Head>
       <Flex alignItems="center" justifyContent="center" height="100vh">
         <Stack backgroundColor="gray.100" p={5} spacing={3}>
-          <Input placeholder="姓名" maxLength={4} />
+          <Input
+            placeholder="姓名"
+            maxLength={4}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
           <RadioGroup onChange={setGender} value={gender}>
             <Stack direction="row">
               <Radio value="girl" colorScheme="pink" id="girl" name="girl">
@@ -56,21 +68,61 @@ export default function Add() {
           </Select>
           <InputGroup>
             <NumberInput>
-              <NumberInputField placeholder="班级" maxLength={2} />
+              <NumberInputField
+                placeholder="班级"
+                maxLength={2}
+                onChange={(event) => {
+                  _setClass(+event.target.value);
+                }}
+              />
             </NumberInput>
             <InputRightAddon>班</InputRightAddon>
           </InputGroup>
-          <Input placeholder="就读高中" maxLength={30} />
+          <Input
+            placeholder="就读高中"
+            maxLength={30}
+            onChange={(event) => {
+              setSchool(event.target.value);
+            }}
+          />
           <InputGroup>
             <InputLeftAddon>QQ</InputLeftAddon>
-            <Input placeholder="QQ" maxLength={20} />
+            <Input
+              placeholder="QQ"
+              maxLength={20}
+              onChange={(event) => {
+                setQQ(event.target.value);
+              }}
+            />
           </InputGroup>
           <InputGroup>
             <InputLeftAddon>微信</InputLeftAddon>
-            <Input placeholder="微信" maxLength={50} />
+            <Input
+              placeholder="微信"
+              maxLength={50}
+              onChange={(event) => {
+                setWechat(event.target.value);
+              }}
+            />
           </InputGroup>
-          <Textarea resize="none" placeholder="对大家说" rows={5} maxLength={800} />
-          <Button colorScheme="blue">登记</Button>
+          <Textarea
+            resize="none"
+            placeholder="说点什么~"
+            rows={5}
+            maxLength={800}
+            onChange={(event) => {
+              setContent(event.target.value);
+            }}
+          />
+          <Button
+            colorScheme="blue"
+            onClick={() => {
+              console.table({ name, gender, year, class: _class, school, qq, wechat, content });
+            }}
+            disabled={!name || !gender || !year || !_class || !school}
+          >
+            登记
+          </Button>
         </Stack>
       </Flex>
     </>
