519632e2b7895502f84bf11b3cc5e3666305c956
jsun969 <i@jsun969.cn>
2021-08-01 19:48:29 +0800

    :mag: Add global head

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="description" content="建平西校同学录" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
