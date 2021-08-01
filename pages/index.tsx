ab6b40c02e6e06c6f25570977adcd3ba4b4caf43
jsun969 <i@jsun969.cn>
2021-08-01 17:41:53 +0800

    :sparkles: Add index basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,30 @@
 import Head from 'next/head';
+import { Input, Heading, Flex, Center, Alert, AlertIcon, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>同学录</title>
-        <meta name="description" content="上海市建平中学西校同学录" />
+        <meta name="description" content="建平西校同学录" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <Flex backgroundColor="gray.100" direction="column" p={3} style={{ position: 'sticky', top: 0 }}>
+          <Center>
+            <Heading my={3}>同学录</Heading>
+          </Center>
+          <Input placeholder="搜索 (姓名/学校)" />
+          <Alert status="info" my={3}>
+            <AlertIcon />
+            没有你的信息?
+            <Text color="teal.500">
+              <NextLink href="/add">点此添加</NextLink>
+            </Text>
+          </Alert>
+        </Flex>
+      </main>
       <footer></footer>
     </>
   );
