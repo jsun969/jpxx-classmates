84c570c3a70df9635eebc8365fd11ab1660d8097
jsun969 <i@jsun969.cn>
2021-08-02 16:30:12 +0800

    :fire: Remove prisma sql temporary file

--- prisma/migrations/20210802082223_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `school` to the `Student` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Student" ADD COLUMN     "school" TEXT NOT NULL;
