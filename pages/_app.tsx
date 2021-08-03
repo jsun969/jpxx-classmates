b9b051e04e0dfdc1e6c3a0cd26a58c0d39b6c630
jsun969 <i@jsun969.cn>
2021-08-03 19:58:33 +0800

    :lipstick: Add github corner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import 'normalize.css';
+import GithubCorner from '../components/GithubCorner';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <GithubCorner />
       <Component {...pageProps} />
     </ChakraProvider>
   );
