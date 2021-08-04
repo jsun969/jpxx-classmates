d0443f01a85e20d6f4485d1335c86952b4ff7e59
jsun969 <i@jsun969.cn>
2021-08-04 17:55:34 +0800

    :art: Add result with json when error

--- pages/api/student.ts
+++ pages/api/student.ts
@@ -3,8 +3,8 @@ import prisma from '../../lib/prisma';
 
 export default async function postStudent(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return res.status(405);
+    return res.status(405).json({ error: 'MethodNotAllowed' });
   }
-  const result = await prisma.student.create({ data: req.body });
-  res.status(201).json(result);
+  await prisma.student.create({ data: req.body });
+  return res.status(201).json({ result: 'Created' });
 }
